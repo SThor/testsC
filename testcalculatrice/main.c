75b0c881a5f6865144750d63f8935c9da3437039
SThor <paulgivel@gmail.com>
2012-12-18 22:22:05 +0100

    on peut maintenant utiliser la "calculatrice" plus d'une fois

--- testcalculatrice/main.c
+++ testcalculatrice/main.c
@@ -3,21 +3,27 @@
 
 int main()
 {
-    int premierNombre = 0, deuxiemeNombre = 0, resultat = 0, reste = 0;
-    printf("Division avec reste : premier nombre ?");
-    scanf("%d", &premierNombre);
-    printf("\nDeuxieme nombre ?");
-    scanf("%d", &deuxiemeNombre);
-    if (deuxiemeNombre != 0)
+    int continuer = 0;
+    do
     {
-        resultat = premierNombre/deuxiemeNombre;
-        reste = premierNombre%deuxiemeNombre;
-        printf("\nQuand on divise %d par %d, il y va %d fois, reste %d \n", premierNombre, deuxiemeNombre, resultat, reste);
-    }
-    else
-    {
-        printf("Desole, les divisions par 0 sont trop difficiles. :( \n");
-
-    }
+        int premierNombre = 0, deuxiemeNombre = 0, resultat = 0, reste = 0;
+        printf("Division avec reste : premier nombre ?");
+        scanf("%d", &premierNombre);
+        printf("\nDeuxieme nombre ?");
+        scanf("%d", &deuxiemeNombre);
+        if (deuxiemeNombre != 0)
+        {
+            resultat = premierNombre/deuxiemeNombre;
+            reste = premierNombre%deuxiemeNombre;
+            printf("\nQuand on divise %d par %d, il y va %d fois, reste %d \n", premierNombre, deuxiemeNombre, resultat, reste);
+        }
+        else
+        {
+            printf("Desole, les divisions par 0 sont trop difficiles. :( \n");
+        }
+        printf("Si vous souhaitez faire une autre division, tapez 1.");
+        scanf("%d", &continuer);
+        printf("\n\n");
+    }while (continuer == 1);
     return 0;
 }
